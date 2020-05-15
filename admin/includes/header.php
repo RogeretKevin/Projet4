a97e38f267ce992bbf7e020f91e2962e81ef3f26
Rogeret Kevin <kevin95660@outlook.fr>
2020-05-16 01:43:44 +0200

    header admin

---
+++ admin/includes/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Blog de Jean Forteroche pour son roman Billet simple pour l'Alaska">
+    <meta name="author" content="Jean Forteroche">
+    <title>Jean Forteroche - Billet simple pour l'Alaska</title>
+
+    <!-- Custom fonts for this template-->
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="../css/my-css.css" rel="stylesheet">
+
+    <!-- WYSIWYG-->
+    <script src="https://cdn.tiny.cloud/1/a96dqabsks4428902mxkaqyasto6gi1qqctqyw5lx2094p6d/tinymce/5/tinymce.min.js"
+        referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: 'textarea'
+        });
+    </script>
+
+    <!-- Alert JS custom -->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="../js/mon-script.js"></script>
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
