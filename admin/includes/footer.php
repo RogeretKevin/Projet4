da00ff23facdc1c96c9dbbddc1239cd91e4e8aa8
Rogeret Kevin <kevin95660@outlook.fr>
2020-05-16 01:45:05 +0200

    footer admin

---
+++ admin/includes/footer.php
@@ -0,0 +1,43 @@
+<!-- Footer -->
+<footer class="fixed-bottom bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Jean Forteroche 2020</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
+
+<!-- FontAwesome -->
+<script src="https://kit.fontawesome.com/900200da29.js" crossorigin="anonymous"></script>
+
+<!-- Bootstrap core JavaScript-->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+</script>
+
+<!-- Custom scripts for all pages-->
+<script src="../js/sb-admin-2.min.js"></script>
+
+<script type="text/javascript" src="../js/jquery.tablesorter.min.js"></script>
+
+<script type="text/javascript">
+
+$(document).ready(function() 
+{ 
+    $("#dataTable").tablesorter(); 
+} 
+); </script>
+
+
+</div>
+<!-- End of Content Wrapper -->
+</body>
+
+</html>
